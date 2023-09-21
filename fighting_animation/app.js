3eb0ac95f2d7d2e16cd80f9cd515757be4f7d71b
salman <alysalman15@gmail.com>
2023-09-21 13:25:16 +0500

    update animation duration

--- fighting_animation/app.js
+++ fighting_animation/app.js
@@ -7,18 +7,28 @@ function abc(event) {
     forward = forward + 20;
     character.style.left = forward + "px";
     character.src = "images/zan-runn.gif";
+    setTimeout(function () {
+      character.src = "images/zankuro-red.gif";
+      character.style.width = "300px";
+      character.style.height = "300px";
+    }, 3000);
   }
   if (event.keyCode === 65) {
     forward = forward - 20;
     character.style.left = forward + "px";
-    character.src = "images/zankuro-badassstrike.gif";
     character.style.width = "400px";
     character.style.height = "400px";
+    character.src = "images/zankuro-badassstrike.gif";
   }
   if (event.keyCode === 87) {
     character.src = "images/zan-die.gif";
     character.style.width = "400px";
     character.style.height = "400px";
+    setTimeout(function () {
+      character.src = "images/zankuro-red.gif";
+      character.style.width = "300px";
+      character.style.height = "300px";
+    }, 1000);
   }
   if (event.keyCode === 90) {
     character.src = "images/zankuro-red.gif";
